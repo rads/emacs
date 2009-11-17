9ee2dc13794ba08fccf6e64648d34ca424b65487
Radford Smith <github.com@radspam.ftml.net>
2009-11-17 17:21:17 -0600

    add restore.rb

---
+++ restore.rb
@@ -0,0 +1,7 @@
+File.open('local.el', 'w') do |f|
+  f.puts 'load ("rads")'
+end
+
+`git submodule update --init`
+`cd vendor/rinari && git submodule update --init`
+puts "Don't forget to M-x byte-compile-file js2"
