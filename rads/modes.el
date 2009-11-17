74f160578254b9dd761861e3fdc8d47be7d729b5
Radford Smith <github.com@radspam.ftml.net>
2009-11-17 16:41:20 -0600

    add yasnippets-rails

--- rads/modes.el
+++ rads/modes.el
@@ -28,6 +28,7 @@
 (vendor 'yasnippet)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
+(yas/load-directory "~/.emacs.d/vendor/yasnippets-rails/rails-snippets")
 
 ;; paredit
 (autoload 'paredit-mode "paredit"
