54dd328a33964f1522dfbbd91980cba1c2b5873a
Radford Smith <github.com@radspam.ftml.net>
2009-11-17 10:06:29 -0600

    add yasnippet

--- rads/modes.el
+++ rads/modes.el
@@ -19,3 +19,9 @@
 ;; go
 ;(add-to-list 'load-path "~/go/misc/emacs" t)
 ;(require 'go-mode-load)
+
+;; yasnippet
+(add-to-list 'load-path "~/.emacs.d/vendor/yasnippet")
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
