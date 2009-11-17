b8fad08fc1cd45615581d2452922a6e5145384f5
Radford Smith <github.com@radspam.ftml.net>
2009-11-17 15:40:30 -0600

    add textmate mode

--- rads/modes.el
+++ rads/modes.el
@@ -25,3 +25,8 @@
 (require 'yasnippet)
 (yas/initialize)
 (yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
+
+;; textmate
+(add-to-list 'load-path "~/.emacs.d/vendor/textmate.el")
+(require 'textmate)
+(textmate-mode)
