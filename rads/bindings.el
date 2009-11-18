d7385e5a16fe88d08432cf3a9bc276094f0ef92c
Radford Smith <github.com@radspam.ftml.net>
2009-11-18 15:35:52 -0600

    make newline and indent default behavior for RET

--- rads/bindings.el
+++ rads/bindings.el
@@ -1,3 +1,5 @@
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
