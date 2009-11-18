f59c8a6c88cec2aa9b731fc29d7f80b10d385d3d
Radford Smith <github.com@radspam.ftml.net>
2009-11-17 22:48:05 -0600

    change shell hook to comint hook so bindings work for SLIME and inf-ruby

--- rads/shell.el
+++ rads/shell.el
@@ -1,8 +1,8 @@
 (add-hook 'shell-mode-hook 'ansi-color-for-comint-mode-on)
 
-(eval-after-load 'shell
-  '(progn
-     (define-key shell-mode-map [up] 'comint-previous-input)
-     (define-key shell-mode-map [down] 'comint-next-input)
-     (define-key shell-mode-map "\C-p" 'comint-previous-input)
-     (define-key shell-mode-map "\C-n" 'comint-next-input)))
+(add-hook 'comint-mode-hook
+          (lambda ()
+            (define-key comint-mode-map [up] 'comint-previous-input)
+            (define-key comint-mode-map [down] 'comint-next-input)
+            (define-key comint-mode-map "\C-p" 'comint-previous-input)
+            (define-key comint-mode-map "\C-n" 'comint-next-input)))
