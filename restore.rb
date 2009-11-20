1fd5fa1bce12cb5b393ef04a92c60ca735460dea
Radford Smith <github.com@radspam.ftml.net>
2009-11-20 15:37:15 -0600

    fix restore.rb

--- restore.rb
+++ restore.rb
@@ -1,7 +1,10 @@
-File.open('local.el', 'w') do |f|
-  f.puts '(load "rads")'
+unless File.exist?('local.el')
+  File.open('local.el', 'w') do |f|
+    f.puts '(load "rads")'
+  end
 end
 
 `git submodule update --init`
 `cd vendor/rinari && git submodule update --init`
 puts "Don't forget to M-x byte-compile-file js2"
+puts "~/Applications/Emacs.app/Contents/MacOS/Emacs --batch --eval '(byte-compile-file \"~/.emacs.d/vendor/js2.el\")'"
