24ed40768b5f736b6d9bad40c0475f841076be7e
Radford Smith <github.com@radspam.ftml.net>
2009-12-19 16:46:31 -0600

    elpa

--- init.el
+++ init.el
@@ -1,7 +1,16 @@
 (add-to-list 'load-path "~/.emacs.d")
- 
+
 ;; You're expected to populate .emacs.d/local.el
 ;; with your own code. This file is under .gitignore
 ;; so it won't be version-controlled. The idea is to
 ;; make this file load other version-controlled files.
 (load "local")
+;;; This was installed by package-install.el.
+;;; This provides support for the package system and
+;;; interfacing with ELPA, the package archive.
+;;; Move this code earlier if you want to reference
+;;; packages in your .emacs.
+(when
+    (load
+     (expand-file-name "~/.emacs.d/elpa/package.el"))
+  (package-initialize))
