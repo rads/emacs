feca53b6d2fcfc368f9b3abe1daea8bbe80713b2
rads <github.com@radspam.ftml.net>
2010-03-16 14:15:28 -0500

    add matlab

--- rads/modes.el
+++ rads/modes.el
@@ -66,3 +66,7 @@
 (load "~/.emacs.d/vendor/haskell-mode/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
+;; matlab
+(vendor 'matlab)
+(require 'matlab-load)
