bb5bf02561670336a44c1f66ff31db50fa7e97b7
Radford Smith <rads@ftml.net>
2011-03-04 02:27:32 -0600

    First version

--- init.el
+++ init.el
@@ -1,7 +1,33 @@
-(add-to-list 'load-path "~/.emacs.d")
+;; Disable mouse interface.
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;; You're expected to populate .emacs.d/local.el
-;; with your own code. This file is under .gitignore
-;; so it won't be version-controlled. The idea is to
-;; make this file load other version-controlled files.
-(load "local")
+;; Set up load path.
+(setq dotfiles-dir (file-name-directory
+                     (or (buffer-file-name) load-file-name)))
+(add-to-list 'load-path dotfiles-dir)
+(add-to-list 'load-path (concat dotfiles-dir "/vendor"))
+
+;; Load system-specific config.
+(load (concat dotfiles-dir "local.el"))
+
+;; Set up ELPA.
+(require 'package)
+(dolist
+  (source '(("technomancy" . "http://repo.technomancy.us/emacs/")
+            ("elpa" . "http://tromey.com/elpa/")))
+  (add-to-list 'package-archives source t))
+(package-initialize)
+
+;; Require basic packages.
+(require 'cl)         ;; Common Lisp Extension
+(require 'saveplace)  ;; Save place between files
+(require 'ffap)       ;; More sensible key bindings for finding files
+(require 'uniquify)   ;; More sensible buffer names
+(require 'ansi-color) ;; ANSI color in shells
+(require 'recentf)    ;; Keep track of recently modified files.
+
+;; Load custom functions.
+(load (concat dotfiles-dir "defuns.el"))
+
