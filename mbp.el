07e8a6d9b43ab71f7ef2b756e4c089c3529dadce
Radford Smith <rads@ftml.net>
2011-03-05 18:05:03 -0600

    Remove files

--- mbp.el
+++
@@ -1,4 +0,0 @@
-(add-to-list 'default-frame-alist '(font . "Consolas-15"))
-(add-to-list 'default-frame-alist '(height . 57))
-(add-to-list 'default-frame-alist '(width . 80))
-(add-to-list 'default-frame-alist '(top . 0))
